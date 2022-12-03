94d6536997d3c45cb53858dd9f9ed6aee46a9793
P-arias <pariasmatos@gmail.com>
2022-12-03 15:20:46 -0500

    login functionalities

--- MovieReviews/urls.py
+++ MovieReviews/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    path('add', views.add, name='add'),
+    path('update/<int:task_id>', views.update, name='update'),
+    path('delete/<int:task_id>', views.delete, name='delete'),
+    path('search', views.search, name='search'),
+
 ]
+
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
